2973ea804b5fc04023166e593610c911d1b7fbf3
Pavlo-Leshtaiev <leshtaev_pavel@yahoo.com>
2019-01-19 13:00:04 +0100

    Change location of the test page

--- src/main/java/esw/edu/trv/view/MainView.java
+++ src/main/java/esw/edu/trv/view/MainView.java
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route
+@Route("test-page")
 public class MainView extends VerticalLayout {
 
     public MainView() {
